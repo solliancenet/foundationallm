e1f2ed6ab2058be75f084c7f4914995eb2d07f75
Ciprian Jichici <ciprian.jichici@genisoft.eu>
2023-08-07 18:59:10 +0300

    Authorization using an API key

--- src/LangChainAPI/main.py
+++ src/LangChainAPI/main.py
@@ -1,9 +1,27 @@
-from fastapi import FastAPI
+from http.client import HTTPException
+from fastapi import FastAPI, Depends
+from fastapi.security import APIKeyHeader
 import uvicorn
+
+import os
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
+
+key_vault_url = os.environ['SOLLIANCEAICOPILOT__LANGCHAINAPI__KEYVAULTURL']
+credential = DefaultAzureCredential()
+secrets_client = SecretClient(key_vault_url, credential=credential)
+api_key_value = secrets_client.get_secret('langchain-api-key').value
+api_key_header = APIKeyHeader(name='X-API-Key')
+
+def api_key_auth(x_api_key: str = Depends(api_key_header)):
+    if x_api_key != api_key_value:
+        raise HTTPException(
+            status_code = 401,
+            detail = 'Invalid API key. You need to provide a valid API key in the X-API-KEY header.')
 
 app = FastAPI()
 
-@app.get('/')
+@app.get('/', dependencies=[Depends(api_key_auth)])
 async def root():
     return { 'message': 'Hello FoundationalLM!' }
 
