ee84c4f8555cb32e0509e634153d280d0c3f0b14
Ciprian Jichici <ciprian.jichici@genisoft.eu>
2023-08-07 15:05:03 +0300

    FastAPI initial version

--- src/LangChainAPI/main.py
+++ src/LangChainAPI/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
 @app.get('/')
 async def root():
     return { 'message': 'Hello FoundationalLM!' }
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='0.0.0.0', port=8765, reload=True)
--- src/LangChainAPI/server.py
+++
@@ -1,4 +0,0 @@
-import uvicorn
-
-if __name__ == '__main__':
-    uvicorn.run('main:app', host='0.0.0.0', port=8765, reload=True)
