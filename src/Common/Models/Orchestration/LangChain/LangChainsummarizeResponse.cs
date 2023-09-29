4f32e8a1465c8957da7f7abc0c3708d8571afd43
Andrei Alistar <andrei.alistar@genisoft.eu>
2023-09-29 13:22:49 +0300

    Added a Common project to the solution

---
+++ src/Common/Models/Orchestration/LangChain/LangChainsummarizeResponse.cs
@@ -0,0 +1,5 @@
+﻿namespace FoundationaLLM.Common.Models.Orchestration.LangChain;
+
+public class LangChainsummarizeResponse : SummarizeRequestBase
+{
+}
---
+++ src/Common/Models/Orchestration/SemanticKernel/SemanticKernelCompletionRequest.cs
@@ -0,0 +1,8 @@
+﻿using FoundationaLLM.Common.Models.Chat;
+
+namespace FoundationaLLM.Common.Models.Orchestration.SemanticKernel;
+
+public class SemanticKernelCompletionRequest : CompletionRequestBase
+{
+    public List<Message> MessageHistory { get; init; } 
+}
