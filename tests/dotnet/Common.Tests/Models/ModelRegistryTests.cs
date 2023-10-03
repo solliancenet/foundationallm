fae498bfcc30f07102d9fc24b7c5de4bea75d910
joelhulen <joelhulen@gmail.com>
2023-10-03 19:33:48 -0400

    Created .NET test projects and created first test under Common.Tests

---
+++ tests/dotnet/Common.Tests/Models/ModelRegistryTests.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FoundationaLLM.Common.Models;
+using FoundationaLLM.Common.Models.Search;
+using NSubstitute;
+
+namespace Common.Tests.Models
+{
+    public class ModelRegistryTests
+    {
+        [Fact]
+        public void IdentifyType_ReturnsCorrectModelType()
+        {
+            var customer = new Customer(
+                "id",
+                "type",
+                "customerId",
+                "title",
+                "firstName",
+                "lastName",
+                "emailAddress",
+                "phoneNumber",
+                "creationDate",
+                new List<CustomerAddress>(),
+                new Password("", ""),
+                0.0
+            );
+            var customerObj = JObject.FromObject(customer);
+            var expectedEntry = ModelRegistry.Models[nameof(Customer)];
+
+            var actualEntry = ModelRegistry.IdentifyType(customerObj);
+
+            Assert.Equal(expectedEntry, actualEntry);
+        }
+    }
+}
