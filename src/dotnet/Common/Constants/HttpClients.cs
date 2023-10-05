7c1b94b9a24959ed37c8e9cd7aeea89d4edae03b
joelhulen <joelhulen@gmail.com>
2023-10-05 07:58:58 -0400

    Update Http Client names and added class comments

--- src/dotnet/Common/Constants/HttpClients.cs
+++ src/dotnet/Common/Constants/HttpClients.cs
@@ -1,9 +1,14 @@
 ﻿namespace FoundationaLLM.Common.Constants;
+
+/// <summary>
+/// Name constants used to configure and retrieve an <see cref="T:System.Net.Http.HttpClient" />,
+/// using <see cref="T:System.Net.Http.IHttpClientFactory" />.
+/// </summary>
 public static class HttpClients
 {
-    public const string DefaultHttpClient = "DefaultHttpClinet";
-    public const string LangChainAPIClient = "LangChainApiClient";
-    public const string SemanticKernelAPIClient = "SemanticKernelApiClient";
-    public const string GatekeeperAPIClient = "GatewayApiClient";
+    public const string DefaultHttpClient = "DefaultHttpClient";
+    public const string LangChainAPIClient = "LangChainAPIClient";
+    public const string SemanticKernelAPIClient = "SemanticKernelAPIClient";
+    public const string GatekeeperAPIClient = "GatekeeperAPIClient";
     public const string AgentFactoryAPIClient = "AgentFactoryAPIClient";
 }
