6224c3d0ce11b8624aef950fca729ba922971786
joelhulen <joelhulen@gmail.com>
2023-10-05 08:37:27 -0400

    Remove LLM services from Core

--- src/dotnet/Core/Interfaces/ISemanticKernelOrchestrationService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FoundationaLLM.Core.Interfaces
-{
-    public interface ISemanticKernelOrchestrationService : ILLMOrchestrationService
-    {
-        Task AddMemory(object item, string itemName, Action<object, float[]> vectorizer);
-
-        Task RemoveMemory(object item);
-    }
-}
--- src/dotnet/CoreAPI/Program.cs
+++ src/dotnet/CoreAPI/Program.cs
@@ -19,43 +19,15 @@ namespace FoundationaLLM.Core.API
             builder.Services.AddOptions<CosmosDbSettings>()
                 .Bind(builder.Configuration.GetSection("FoundationaLLM:CosmosDB"));
 
-            builder.Services.AddOptions<SemanticKernelOrchestrationServiceSettings>()
-                .Bind(builder.Configuration.GetSection("FoundationaLLM:SemanticKernelOrchestration"));
-
-            builder.Services.AddOptions<LangChainOrchestrationServiceSettings>()
-                .Bind(builder.Configuration.GetSection("FoundationaLLM:LangChainOrchestration"));
-
             builder.Services.AddOptions<ChatServiceSettings>()
                 .Bind(builder.Configuration.GetSection("FoundationaLLM:Chat"));
 
             builder.Services.AddSingleton<ICosmosDbService, CosmosDbService>();
-            builder.Services.AddSingleton<ISemanticKernelOrchestrationService, SemanticKernelOrchestrationService>();
-            builder.Services.AddSingleton<ILangChainOrchestrationService, LangChainOrchestrationService>();
             builder.Services.AddSingleton<ICoreService, CoreService>();
             builder.Services.AddSingleton<IGatekeeperAPIService, GatekeeperAPIService>();
 
             builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
 
-            builder.Services
-                .AddHttpClient(HttpClients.LangChainAPIClient,
-                    httpClient =>
-                    {
-                        httpClient.BaseAddress = new Uri(builder.Configuration["FoundationaLLM:LangChainOrchestration:APIUrl"]);
-                        httpClient.DefaultRequestHeaders.Add("X-API-KEY", builder.Configuration["FoundationaLLM:LangChainOrchestration:APIKey"]);
-                    })
-                .AddTransientHttpErrorPolicy(policyBuilder =>
-                    policyBuilder.WaitAndRetryAsync(
-                        3, retryNumber => TimeSpan.FromMilliseconds(600)));
-            builder.Services
-                .AddHttpClient(HttpClients.SemanticKernelAPIClient,
-                    httpClient =>
-                    {
-                        httpClient.BaseAddress = new Uri(builder.Configuration["FoundationaLLM:SemanticKernelOrchestration:APIUrl"]);
-                        httpClient.DefaultRequestHeaders.Add("X-API-KEY", builder.Configuration["FoundationaLLM:SemanticKernelOrchestration:APIKey"]);
-                    })
-                .AddTransientHttpErrorPolicy(policyBuilder =>
-                    policyBuilder.WaitAndRetryAsync(
-                        3, retryNumber => TimeSpan.FromMilliseconds(600)));
             builder.Services
                 .AddHttpClient(HttpClients.GatekeeperAPIClient,
                     httpClient =>
