9d83f74c2fc2b79459b845e1004d52c6598e8c36
joelhulen <joelhulen@gmail.com>
2023-10-05 08:36:18 -0400

    Add LLM interfaces, services, and models to AgentFactory

---
+++ src/dotnet/AgentFactory/Models/ConfigurationOptions/SemanticKernelOrchestrationServiceSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace FoundationaLLM.AgentFactory.Models.ConfigurationOptions
+{
+    public class SemanticKernelOrchestrationServiceSettings
+    {
+        public string? APIUrl { get; init; }
+        public string? APIKey { get; init; }
+    }
+}
