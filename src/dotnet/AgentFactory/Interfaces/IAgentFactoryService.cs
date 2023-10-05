31bb0638b42604912b6733fd6af85ed26958f4ab
joelhulen <joelhulen@gmail.com>
2023-10-05 09:17:49 -0400

    Add AgentFactory service

--- src/dotnet/AgentFactory/Interfaces/IAgentFactoryService.cs
+++ src/dotnet/AgentFactory/Interfaces/IAgentFactoryService.cs
@@ -1,6 +1,19 @@
-﻿namespace FoundationaLLM.AgentFactory.Core.Interfaces;
+﻿using FoundationaLLM.Common.Models.Orchestration;
+
+namespace FoundationaLLM.AgentFactory.Core.Interfaces;
 
 public interface IAgentFactoryService
 {
-    
+    bool SetLLMOrchestrationPreference(string orchestrationService);
+    string Status { get; }
+
+    /// <summary>
+    /// Retrieve a completion from the configured orchestration service.
+    /// </summary>
+    Task<CompletionResponseBase> GetCompletion(CompletionRequestBase completionRequest);
+
+    /// <summary>
+    /// Retrieve a summarization for the passed in prompt from the orchestration service.
+    /// </summary>
+    Task<string> GetSummary(string content);
 }
---
+++ src/dotnet/AgentFactory/Models/ConfigurationOptions/ChatServiceSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoundationaLLM.AgentFactory.Models.ConfigurationOptions
+{
+    public record ChatServiceSettings
+    {
+        public required string DefaultOrchestrationService { get; init; }
+    }
+}
