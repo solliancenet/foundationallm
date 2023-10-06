4d163aa4ce1f679171845bf47c29f577fd889629
joelhulen <joelhulen@gmail.com>
2023-10-06 18:31:35 -0400

    Implement Entra auth to Blazor app

--- src/ui/Chat/Program.cs
+++ src/ui/Chat/Program.cs
@@ -1,4 +1,10 @@
 using FoundationaLLM.Chat.Helpers;
+using FoundationaLLM.Common.Services;
+using FoundationaLLM.Core.Interfaces;
+using FoundationaLLM.Core.Services;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
 using Polly;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,11 +18,37 @@ builder.Services.AddHttpClient(FoundationaLLM.Common.Constants.HttpClients.Defau
         policyBuilder.WaitAndRetryAsync(
             3, retryNumber => TimeSpan.FromMilliseconds(600)));
 
+var keyVaultUri = builder.Configuration["FoundationaLLM:Configuration:KeyVaultUri"];
+builder.Services.AddSingleton<FoundationaLLM.Common.Interfaces.IConfiguration>(new KeyVaultConfiguration(keyVaultUri));
+
+var serviceProvider = builder.Services.BuildServiceProvider();
+var kvConfig = serviceProvider.GetRequiredService<FoundationaLLM.Common.Interfaces.IConfiguration>();
+
+var azureAdSecret = kvConfig.GetValue(builder.Configuration["FoundationaLLM:Entra:ClientSecretKeyName"]);
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(options =>
+    {
+        options.ClientSecret = azureAdSecret;
+        options.Instance = builder.Configuration["FoundationaLLM:Entra:Instance"];
+        options.TenantId = builder.Configuration["FoundationaLLM:Entra:TenantId"];
+        options.ClientId = builder.Configuration["FoundationaLLM:Entra:ClientId"];
+        //options.Scope.Add(builder.Configuration["FoundationaLLM:Entra:Scopes"]);
+        options.CallbackPath = builder.Configuration["FoundationaLLM:Entra:CallbackPath"];
+    });
+builder.Services.AddControllersWithViews()
+    .AddMicrosoftIdentityUI();
+builder.Services.AddAuthorization(options =>
+{
+    // By default, all incoming requests will be authorized according to the default policy
+    options.FallbackPolicy = options.DefaultPolicy;
+});
+
 builder.RegisterConfiguration();
 builder.Services.AddRazorPages();
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
-builder.Services.AddServerSideBlazor();
+builder.Services.AddServerSideBlazor()
+    .AddMicrosoftIdentityConsentHandler();
 builder.Services.RegisterServices();
 
 var app = builder.Build();
@@ -30,6 +62,9 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
