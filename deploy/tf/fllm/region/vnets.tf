41a88bdcec406084f58eaa0600f82da1301dfdfc
Matthew Alan Gray <matthew.gray@hatboysoftware.com>
2023-10-09 16:43:26 -0500

    Creating OpenAI instances

--- deploy/tf/fllm/region/vnets.tf
+++ deploy/tf/fllm/region/vnets.tf
@@ -26,6 +26,4 @@ resource "azurerm_subnet" "subnets" {
   name                 = each.key
   resource_group_name  = azurerm_resource_group.rgs["NET"].name
   virtual_network_name = azurerm_virtual_network.vnet.name
-
-  tags = local.tags
 }
