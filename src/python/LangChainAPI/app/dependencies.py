fa384a7f2d8577b13ec14f7f05046329f6f76d89
kylebunting <kyle.bunting@gmail.com>
2023-10-09 16:12:32 -0600

    Fixing injection of Configuration into validate_api_key_header

--- src/python/LangChainAPI/app/dependencies.py
+++ src/python/LangChainAPI/app/dependencies.py
@@ -1,10 +1,11 @@
+from typing import Annotated
 from fastapi import Depends, HTTPException
 from fastapi.security import APIKeyHeader
 
 from foundationallm.auth import APIKeyValidator
 from foundationallm.config import Configuration
    
-async def validate_api_key_header(x_api_key: str = Depends(APIKeyHeader(name='X-API-Key'))):
+async def validate_api_key_header(app_config: Annotated[Configuration, Depends(Configuration)], x_api_key: str = Depends(APIKeyHeader(name='X-API-Key'))):
     """
     Validates that the X-API-Key value in the request header matches
     the key expected for this API.
@@ -19,7 +20,7 @@ async def validate_api_key_header(x_api_key: str = Depends(APIKeyHeader(name='X-
     
     result = APIKeyValidator(
         api_key_name='foundationallm-langchain-api-key',
-        config=Configuration()
+        config=app_config
     ).validate_api_key(x_api_key)
     
     if not result:
