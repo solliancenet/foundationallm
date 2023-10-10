7053279badb1442da2cc74b70f8f6a97bd54e4d8
Carey Payette <carey.payette@gmail.com>
2023-10-09 22:55:06 -0400

    LangChainAPI tested and working

--- src/python/LangChainAPI/app/dependencies.py
+++ src/python/LangChainAPI/app/dependencies.py
@@ -1,22 +1,24 @@
 from typing import Annotated
 from fastapi import Depends, HTTPException
 from fastapi.security import APIKeyHeader
-
+from foundationallm.auth import AzureCredential
 from foundationallm.auth import APIKeyValidator
 from foundationallm.config import Configuration
    
-async def validate_api_key_header(app_config: Annotated[Configuration, Depends(Configuration)], x_api_key: str = Depends(APIKeyHeader(name='X-API-Key'))):
+async def validate_api_key_header(x_api_key: str = Depends(APIKeyHeader(name='X-API-Key'))):
     """
     Validates that the X-API-Key value in the request header matches
     the key expected for this API.
     
     Parameters
-    ----------
-    - app_config: Configuration
-        The configuration object used to retrieve application configuration values.
+    ----------    
     - x_api_key : str
         The X-API-Key value in the request header.
     """
+    
+    credential = AzureCredential().get_credential()     
+    key_vault_name = Configuration().get_value(key="foundationallm-keyvault-name")  
+    app_config = Configuration(keyvault_name=key_vault_name, credential=credential)
     
     result = APIKeyValidator(
         api_key_name='foundationallm-langchain-api-key',
