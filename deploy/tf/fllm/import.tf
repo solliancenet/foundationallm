78609e7cf73ccccd341600409f64e06236da17bf
Jim Counts <jamesrcounts@outlook.com>
2023-10-11 19:22:43 +0000

    Import APIM

---
+++ deploy/tf/fllm/import.tf
@@ -0,0 +1,4 @@
+import {
+    to = module.regions["East US"].azurerm_api_management.openai_apim
+    id = "/subscriptions/4dae7dc4-ef9c-4591-b247-8eacb27f3c9e/resourceGroups/EUS-FLLM-DEMO-OAI-rg/providers/Microsoft.ApiManagement/service/EUS-FLLM-DEMO-OAI-apim"
+}
