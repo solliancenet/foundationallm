c67c563c89eb9c8061deeb45f1b43c3135942e0d
joelhulen <joelhulen@gmail.com>
2023-10-11 12:27:17 -0400

    Added no-op user claims provider for services that do not incorporate claims-based auth

---
+++ src/dotnet/Common/Services/NoOpUserClaimsProviderService.cs
@@ -0,0 +1,28 @@
+﻿using FoundationaLLM.Common.Interfaces;
+using FoundationaLLM.Common.Models.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoundationaLLM.Common.Services
+{
+    /// <summary>
+    /// No-op implementation of <see cref="IUserClaimsProviderService"/> in cases
+    /// where the user identity is not needed.
+    /// </summary>
+    public class NoOpUserClaimsProviderService : IUserClaimsProviderService
+    {
+        /// <summary>
+        /// Returns null.
+        /// </summary>
+        /// <param name="userPrincipal"></param>
+        /// <returns></returns>
+        public UnifiedUserIdentity? GetUserIdentity(ClaimsPrincipal? userPrincipal)
+        {
+            return null;
+        }
+    }
+}
