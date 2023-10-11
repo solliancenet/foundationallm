3035e4b82ffb9ee55393fd998a42e57a5614ff84
Carey Payette <carey.payette@gmail.com>
2023-10-11 16:02:41 -0400

    Updated AgentMetadata

---
+++ src/python/PythonSDK/foundationallm/hubs/agent/language_model_provider.py
@@ -0,0 +1,4 @@
+from enum import Enum, auto
+
+class LanguageModelProvider(Enum):
+    AZURE = auto()
---
+++ src/python/PythonSDK/foundationallm/hubs/agent/language_model_type.py
@@ -0,0 +1,4 @@
+from enum import Enum, auto
+
+class LanguageModelType(Enum):
+    OPENAI = auto()
