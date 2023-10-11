926a1f9bbfb2622e224a55402dd7d4fb30f4e84b
joelhulen <joelhulen@gmail.com>
2023-10-10 21:56:55 -0400

    Created constants for HTTP headers

---
+++ src/dotnet/Common/Constants/HttpHeaders.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoundationaLLM.Common.Constants
+{
+    /// <summary>
+    /// Common HTTP headers used throughout the FoundationaLLM infrastructure..
+    /// </summary>
+    public static class HttpHeaders
+    {
+        /// <summary>
+        /// API key header used by APIs to authenticate requests.
+        /// </summary>
+        public const string APIKey = "X-API-Key";
+        /// <summary>
+        /// User identity header used by APIs to pass user identity information.
+        /// </summary>
+        public const string UserIdentity = "X-USER-IDENTITY";
+    }
+}
