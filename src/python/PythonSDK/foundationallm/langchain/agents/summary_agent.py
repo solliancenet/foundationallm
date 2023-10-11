9d6bfd9670024b0139722d1a18e78fbc16285a42
joelhulen <joelhulen@gmail.com>
2023-10-11 09:37:02 -0400

    Fixed typo

--- src/python/PythonSDK/foundationallm/langchain/agents/summary_agent.py
+++ src/python/PythonSDK/foundationallm/langchain/agents/summary_agent.py
@@ -1,4 +1,4 @@
-woimport tiktoken
+import tiktoken
 from langchain.chains.summarize import load_summarize_chain
 from langchain.docstore.document import Document
 from langchain.text_splitter import CharacterTextSplitter
