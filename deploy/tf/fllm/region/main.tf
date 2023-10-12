195e16dfb28d04d19d0198bc08d015f1e3ae1211
Jim Counts <jamesrcounts@outlook.com>
2023-10-12 18:26:35 +0000

    Move resource groups

--- deploy/tf/fllm/region/main.tf
+++ deploy/tf/fllm/region/main.tf
@@ -17,6 +17,14 @@ resource "azurerm_monitor_action_group" "do_nothing" {
   tags                = local.tags
 }
 
+resource "azurerm_resource_group" "rgs" {
+  for_each = local.resource_groups
+
+  location = local.location
+  name     = join("-", [local.resource_prefix, each.key, "rg"])
+  tags     = merge(each.value.tags, local.tags)
+}
+
 module "ampls" {
   source = "./modules/monitor-private-link-scope"
 
--- deploy/tf/fllm/region/resource_groups.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_resource_group" "rgs" {
-  for_each = local.resource_groups
-
-  location = local.location
-  name     = join("-", [local.resource_prefix, each.key, "rg"])
-  tags     = merge(each.value.tags, local.tags)
-}
