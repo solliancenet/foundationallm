68bf59183dec458dc75fb358791a4a64dead3ae0
kylebunting <kyle.bunting@gmail.com>
2023-10-13 00:57:19 -0600

    Adding inline documentation to the LangChainAPI + removing summary endpoint

--- src/python/LangChainAPI/app/dependencies.py
+++ src/python/LangChainAPI/app/dependencies.py
@@ -7,21 +7,35 @@ from foundationallm.config import Configuration
 def get_config(credential: Annotated[AzureCredential, Depends(AzureCredential)]):
     """
     Gets a configuration object for retrieving application configuration values.
+    
+    Parameters
+    ----------
+    credential : AzureCredential
+        The credential required to authenticate against Azure Key Vault.
+        
+    Returns
+    Configuration
+        Returns a Configuration object used for access Azure Key Vault and application settings.
     """
+    
     keyvault_name = Configuration().get_value(key='foundationallm-keyvault-name')
     return Configuration(keyvault_name=keyvault_name, credential=credential)
 
 async def validate_api_key_header(app_config: Annotated[Configuration, Depends(get_config)], x_api_key: str = Depends(APIKeyHeader(name='X-API-Key'))):
     """
-    Validates that the X-API-Key value in the request header matches
-    the key expected for this API.
+    Validates that the X-API-Key value in the request header matches the key expected for this API.
     
     Parameters
     ----------
-    - app_config: Configuration
+    app_config : Configuration
         Used for retrieving application configuration settings.
-    - x_api_key : str
+    x_api_key : str
         The X-API-Key value in the request header.
+        
+    Returns
+    bool
+        Returns True of the X-API-Key value from the request header matches the expected value.
+        Otherwise, returns False.
     """
 
     result = x_api_key == app_config.get_value('foundationallm-langchain-api-key')
--- src/python/LangChainAPI/app/routers/orchestration.py
+++ src/python/LangChainAPI/app/routers/orchestration.py
@@ -18,10 +18,19 @@ router = APIRouter(
 
 @router.post('/completion')
 async def get_completion(completion_request: CompletionRequest) -> CompletionResponse:
+    """
+    Retrieves a completion from a language model
+    
+    Parameters
+    ----------
+    completion_request : CompletionRequest
+        The request object containing data required to generate a completion.
+
+    Returns
+    -------
+    CompletionResponse
+        Object containing the completion and token usage details
+    """
+    
     completion_agent = AgentFactory(request = completion_request, config = app_config).get_agent()
     return completion_agent.run()
-    
-@router.post('/summary')
-async def get_summary(summary_request: SummaryRequest) -> SummaryResponse:
-    summary_agent = AgentFactory(request = summary_request, config = app_config).get_agent()
-    return summary_agent.run()
