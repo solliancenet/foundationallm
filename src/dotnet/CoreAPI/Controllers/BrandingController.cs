95234f3bdcf5fd4b4f00fcacab0627d0e10ad34c
joelhulen <joelhulen@gmail.com>
2023-10-16 08:56:10 -0400

    Add the branding controller

---
+++ src/dotnet/CoreAPI/Controllers/BrandingController.cs
@@ -0,0 +1,30 @@
+﻿using Asp.Versioning;
+using FoundationaLLM.Common.Models.Configuration.Branding;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace FoundationaLLM.Core.API.Controllers
+{
+    [Authorize]
+    [Authorize(Policy = "RequiredScope")]
+    [ApiVersion(1.0)]
+    [ApiController]
+    [Route("[controller]")]
+    public class BrandingController : ControllerBase
+    {
+        private readonly ClientBrandingConfiguration _settings;
+
+        public BrandingController(IOptions<ClientBrandingConfiguration> settings)
+        {
+            _settings = settings.Value;
+        }
+
+        [AllowAnonymous]
+        [HttpGet(Name = "GetBranding")]
+        public IActionResult Index()
+        {
+            return Ok(_settings);
+        }
+    }
+}
