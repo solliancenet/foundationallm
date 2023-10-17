32c414e5640c8ec49e11e84e0d1a1c8465899d2f
joelhulen <joelhulen@gmail.com>
2023-10-16 23:23:40 -0400

    Create branding service and manager

---
+++ src/ui/Chat/Helpers/BrandingService.cs
@@ -0,0 +1,35 @@
+﻿using FoundationaLLM.Common.Models.Configuration.Branding;
+using Microsoft.Identity.Web;
+
+namespace FoundationaLLM.Chat.Helpers
+{
+    public class BrandingService : IBrandingService
+    {
+        /// <inheritdoc/>
+        public ClientBrandingConfiguration? CurrentConfig { get; private set; }
+        private readonly MicrosoftIdentityConsentAndConditionalAccessHandler _consentHandler;
+        private readonly IBrandManager _brandManager;
+
+        public BrandingService(IBrandManager brandManager,
+            MicrosoftIdentityConsentAndConditionalAccessHandler consentHandler)
+        {
+            _brandManager = brandManager;
+            _consentHandler = consentHandler;
+        }
+
+        /// <inheritdoc/>
+        public async Task InitializeAsync()
+        {
+            try
+            {
+                CurrentConfig = await _brandManager.GetBrandAsync();
+            }
+            catch (Exception e)
+            {
+                _consentHandler.HandleException(e);
+                throw;
+            }
+        }
+    }
+
+}
