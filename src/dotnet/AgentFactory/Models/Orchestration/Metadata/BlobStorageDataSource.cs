b4b5c48efe4f475918ee63a9067390880d9fdea0
Chris Givens <givenscj@Hotmail.com>
2023-10-18 12:29:53 -0400

    conflict fix

---
+++ src/dotnet/AgentFactory/Models/Orchestration/Metadata/BlobStorageDataSource.cs
@@ -0,0 +1,22 @@
+﻿using FoundationaLLM.AgentFactory.Core.Models.Orchestration.DataSourceConfigurations;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoundationaLLM.AgentFactory.Core.Models.Orchestration.Metadata
+{
+    /// <summary>
+    /// Blob storage data source metadata model.
+    /// </summary>
+    public class BlobStorageDataSource : MetadataBase
+    {
+        /// <summary>
+        /// Blob storage configuration settings.
+        /// </summary>
+        [JsonProperty("configuration")]
+        public BlobStorageConfiguration Configuration { get; set; }
+    }
+}
