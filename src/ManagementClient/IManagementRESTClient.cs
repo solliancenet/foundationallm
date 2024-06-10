411a77cbd6d7fcab066cd5d8f63d085f25e9b0ba
Brian Noyes <brian.noyes@solliance.net>
2024-06-10 10:18:55 -0700

    Added REST client methods for status and management of resources

--- src/ManagementClient/IManagementRESTClient.cs
+++ src/ManagementClient/IManagementRESTClient.cs
@@ -14,6 +14,30 @@ namespace FoundationaLLM.Client.Management
         /// </summary>
         /// <returns></returns>
         Task<bool> IsAuthenticatedAsync();
-        Task<>
+        /// <summary>
+        /// Retrieves a collection of resources of a given type
+        /// from the resource provider for a specific resource type
+        /// </summary>
+        /// <typeparam name="T">The resource type</typeparam>
+        /// <returns></returns>
+        Task<List<T>> GetResources<T>(string instanceId, string resourceProvider, string resourcePath);
+        /// <summary>
+        /// Upserts a resource
+        /// </summary>
+        /// <typeparam name="T">The resource type</typeparam>
+        /// <param name="resource">The resource object to insert or update.</param>
+        /// <returns>The persisted resource</returns>
+        /// <param name="instanceId">The instance Id of the resource.</param>
+        /// <param name="resourceProvider">The resource provider for the resource type being upserted</param>
+        /// <param name="resourcePath">The path given to the resource provider to locate and update the resource</param>
+        Task<T> UpsertResource<T>(T resource, string instanceId, string resourceProvider, string resourcePath);
+        /// <summary>
+        /// Deletes a resource identified by the input parameters
+        /// </summary>
+        /// <param name="instanceId">The instance Id of the resource.</param>
+        /// <param name="resourceProvider">The resource provider for the resource type being deleted</param>
+        /// <param name="resourcePath">The path given to the resource provider to locate and delete the resource</param>
+        /// <returns></returns>
+        Task DeleteResource(string instanceId, string resourceProvider, string resourcePath);
     }
 }
--- src/ManagementClient/ManagementRESTClient.cs
+++ src/ManagementClient/ManagementRESTClient.cs
@@ -1,4 +1,6 @@
-﻿using FoundationaLLM.Common.Models.Infrastructure;
+﻿using Azure;
+using FoundationaLLM.Common.Models.Infrastructure;
+using System.Text;
 using System.Text.Json;
 
 namespace FoundationaLLM.Client.Management
@@ -9,7 +11,6 @@ namespace FoundationaLLM.Client.Management
     public class ManagementRESTClient : IManagementRESTClient
     {
         private HttpClient _httpClient = new HttpClient();
-
         /// <summary>
         /// Constructs the client that calls the Management API with a given base address
         /// </summary>
@@ -30,6 +31,33 @@ namespace FoundationaLLM.Client.Management
         {
             var response = await _httpClient.GetAsync("/status/auth");
             return response.IsSuccessStatusCode;
+        }
+
+        /// <inheritdoc/>
+        public async Task<List<T>> GetResources<T>(string instanceId, string resourceProvider, string resourcePath)
+        {
+            var response = await _httpClient.GetAsync($"instances/{instanceId}/providers/{resourceProvider}/{resourcePath}");
+            response.EnsureSuccessStatusCode();
+            var responseContent = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<List<T>>(responseContent)!;
+
+        }
+        /// <inheritdoc/>
+        public async Task<T> UpsertResource<T>(T resource, string instanceId, string resourceProvider, string resourcePath)
+        {
+            string jsonContent = JsonSerializer.Serialize(resource);
+
+            var response = await _httpClient.PostAsync($"instances/{instanceId}/providers/{resourceProvider}/{resourcePath}",
+                new StringContent(jsonContent, Encoding.UTF8, "application/json"));
+            response.EnsureSuccessStatusCode();
+            var responseContent = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<T>(responseContent)!;
+        }
+
+        /// <inheritdoc/>
+        public async Task DeleteResource(string instanceId, string resourceProvider, string resourcePath)
+        {
+            var response = await _httpClient.DeleteAsync($"instances/{instanceId}/providers/{resourceProvider}/{resourcePath}");
         }
     }
 }
